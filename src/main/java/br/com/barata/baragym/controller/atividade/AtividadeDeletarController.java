3f36c79dc023e9bd7c4681c29da319d4c24d75bb
Pedro Barata <pedrofelipebarata@gmail.com>
2022-07-07 19:48:57 -0300

    fix(delete): Remove os metodos duplicados

--- src/main/java/br/com/barata/baragym/controller/atividade/AtividadeDeletarController.java
+++ src/main/java/br/com/barata/baragym/controller/atividade/AtividadeDeletarController.java
@@ -1,15 +1,15 @@
 package br.com.barata.baragym.controller.atividade;
 
-import br.com.barata.baragym.controller.atividade.request.AtividadeRequest;
 import br.com.barata.baragym.controller.usuario.response.converter.UsuarioResponseConverter;
-import br.com.barata.baragym.model.Atividade;
 import br.com.barata.baragym.service.AtividadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 @RestController
@@ -21,12 +21,6 @@ public class AtividadeDeletarController {
 
  @Autowired
  private UsuarioResponseConverter converter;
-
- @PostMapping
- @PreAuthorize("hasAnyRole('ADMIN')")
- public Atividade criaAtividade(@Valid @RequestBody AtividadeRequest request) {
-  return atividadeService.criarAtividade(request);
- }
 
  @DeleteMapping("/{atividadeId}")
  @PreAuthorize("hasAnyRole('ADMIN')")
--- src/main/java/br/com/barata/baragym/controller/turma/TurmaDeletarController.java
+++ src/main/java/br/com/barata/baragym/controller/turma/TurmaDeletarController.java
@@ -1,15 +1,15 @@
 package br.com.barata.baragym.controller.turma;
 
-import br.com.barata.baragym.controller.turma.request.TurmaRequest;
 import br.com.barata.baragym.controller.usuario.response.converter.UsuarioResponseConverter;
-import br.com.barata.baragym.model.Turma;
 import br.com.barata.baragym.service.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 @RestController
@@ -21,12 +21,6 @@ public class TurmaDeletarController {
 
  @Autowired
  private UsuarioResponseConverter converter;
-
- @PostMapping
- @PreAuthorize("hasAnyRole('ADMIN')")
- public Turma criaTurma(@Valid @RequestBody TurmaRequest request) {
-  return turmaService.criarTurma(request);
- }
 
  @DeleteMapping("/{turmaId}")
  @PreAuthorize("hasAnyRole('ADMIN')")
